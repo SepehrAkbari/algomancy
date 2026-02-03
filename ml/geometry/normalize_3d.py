7d22e7fa52f0398f16490553de57c79b6c6408c0
Sepehr Akbari <isepehrakbari@gmail.com>
2026-02-03 16:56:49 -0600

    Update normalize_3d.py

--- ml/geometry/normalize_3d.py
+++ ml/geometry/normalize_3d.py
@@ -10,19 +10,26 @@ def normalize_3d(v):
     """
     v = np.asarray(v, dtype=float)
     
-    if v.shape == (3,):
-        v = v.reshape(1, 3)
-    elif len(v.shape) != 2 or v.shape[1] != 3:
+    if v.ndim == 1:
+        if v.shape[0] != 3: 
+            raise ValueError
+        v_hat = v.reshape(1, 3)
+    elif v.ndim == 2:
+        if v.shape[1] != 3: 
+            raise ValueError
+        v_hat = v
+    else:
         raise ValueError
-    
-    norms = np.linalg.norm(v, axis=1, keepdims=True)
-    norms = np.where(norms < 1e-10, 1, norms)
-    v_norm = v / norms
 
-    if v.shape == (3,):
-        v_norm = v_norm.reshape(3,)
-        
-    return v_norm
+    norms = np.linalg.norm(v_hat, axis=1, keepdims=True)
+    
+    mask = (norms > 1e-10).flatten()
+    
+    res = np.zeros_like(v_hat)
+    res[mask] = v_hat[mask] / norms[mask]
+    
+    return res.reshape(v.shape)
+
 
 if __name__ == "__main__":
     v = [[0, 0, 0], [1, 2, 2]]
